520fc8781c1b269ec4d7338fadf855e4b195fc59
Rock de Vocht <rock@simsage.nz>
2020-05-25 23:25:24 +0100

    added requirements and download

---
+++ download.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd original_data/
+
+if [ ! -f "train-v1.1.json" ]; then
+  curl -O https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json
+else
+  printf "train v1.1 already downloaded\n"
+fi
+
+if [ ! -f "dev-v1.1.json" ]; then
+  curl -O https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json
+else
+  printf "dev v1.1. already downloaded\n"
+fi
+
+if [ ! -f "glove.840B.300d.zip" ]; then
+  curl -O http://downloads.cs.stanford.edu/nlp/data/glove.840B.300d.zip
+else
+  printf "glove 840B 300d zip already downloaded\n"
+fi
+
+if [ ! -f "glove.840B.300d.txt" ]; then
+  printf "unzipping global 840B 300d zip\n"
+  unzip glove.840B.300d.zip
+else
+  printf "glove 840B 300d txt already extracted\n"
+fi
+
